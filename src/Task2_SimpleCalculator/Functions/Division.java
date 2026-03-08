7218cdabeb0a6699685f72b4dd6a818f9bd6dc79
Saunders <sebuwei1@umbc.edu>
2026-03-08 14:03:36 -0400

    Division method added

---
+++ src/Task2_SimpleCalculator/Functions/Division.java
@@ -0,0 +1,7 @@
+package Task2_SimpleCalculator.Functions;
+
+public class Division {
+    public static int divide(int a, int b){
+        return a / b;
+    }
+}
--- src/Task2_SimpleCalculator/Main.java
+++ src/Task2_SimpleCalculator/Main.java
@@ -1,9 +1,6 @@
 package Task2_SimpleCalculator;
 
-import Task2_SimpleCalculator.Functions.Add;
-import Task2_SimpleCalculator.Functions.Multiplication;
-import Task2_SimpleCalculator.Functions.Square;
-import Task2_SimpleCalculator.Functions.Subtract;
+import Task2_SimpleCalculator.Functions.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,11 +9,13 @@ public class Main {
         int product = Multiplication.multiply(4, 4);
         int squared = Square.square(2);
         int difference = Subtract.subtract(10, 5);
+        int division = Division.divide(4,2);
 
         // Printouts
         System.out.println("Addition: " + sum);
         System.out.println("Product: " + product);
         System.out.println("Squared: " + squared);
         System.out.println("Difference: " + difference);
+        System.out.println("Division: " + division);
     }
 }
