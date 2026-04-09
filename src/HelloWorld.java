f55654e7ccaca947a09178b77471eef00288f0af
Gavin Williams <gavinw2@umbc.edu>
2026-04-09 16:21:22 -0400

    test commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,20 @@
+
+
+
 public class HelloWorld {
+    /**
+    *This is the main method
+    *
+    * @param args allows the program to print the statement
+    *
+     */
     public static void main(String[] args) {
+
+        /**
+        *This code down here prints out Hello, docker file java!
+        *
+        *
+        */
         System.out.println("Hello, Docker and Java!");
         System.out.println("My name is Aaron. Nice to meet you!");
     }
